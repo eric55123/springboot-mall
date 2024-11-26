7efa029702aa90d555aef6b0248c318b830e6f97
eric55123 <ken0970754487@gmail>
2024-11-26 17:43:52 +0800

    商品功能 - 實做查詢商品列表(查詢條件)

--- springboot-mall/src/main/java/com/eric/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/eric/springbootmall/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.eric.springbootmall.service;
 
+import com.eric.springbootmall.constant.ProductCategory;
 import com.eric.springbootmall.dto.ProductRequest;
 import com.eric.springbootmall.model.Product;
 
@@ -8,7 +9,7 @@ import java.util.List;
 
 public interface ProductService {
 
-    List<Product> getProducts();
+    List<Product> getProducts(ProductCategory category, String search);
 
     Product getProductById(Integer productId);
 
--- springboot-mall/src/main/java/com/eric/springbootmall/service/impl/ProductServiceImpl.java
+++ springboot-mall/src/main/java/com/eric/springbootmall/service/impl/ProductServiceImpl.java
@@ -1,6 +1,7 @@
 package com.eric.springbootmall.service.impl;
 
 
+import com.eric.springbootmall.constant.ProductCategory;
 import com.eric.springbootmall.dao.ProductDao;
 import com.eric.springbootmall.dto.ProductRequest;
 import com.eric.springbootmall.model.Product;
@@ -17,8 +18,8 @@ public class ProductServiceImpl implements ProductService {
     private ProductDao productDao;
 
     @Override
-    public List<Product> getProducts() {
-        return productDao.getProducts();
+    public List<Product> getProducts(ProductCategory category, String search) {
+        return productDao.getProducts(category, search);
     }
 
     @Override
