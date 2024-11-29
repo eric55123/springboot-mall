76b539cdb1629da8bcdc61c6dcada796b28115af
eric55123 <ken0970754487@gmail>
2024-11-29 16:49:18 +0800

    商品功能 - 實做查詢商品列表 (排序)

--- springboot-mall/src/main/java/com/eric/springbootmall/dao/impl/ProductDaoImpl.java
+++ springboot-mall/src/main/java/com/eric/springbootmall/dao/impl/ProductDaoImpl.java
@@ -26,26 +26,26 @@ public class ProductDaoImpl implements ProductDao {
 
     @Override
     public List<Product> getProducts(ProductQueryParams productQueryParams) {
-        String sql = "SELECT product_id, product_name, category, image_url, price, stock, description," +
-                " created_date, last_modified_date FROM product WHERE 1=1";
+        String sql = "SELECT product_id, product_name, category, image_url, price, stock, description, " +
+                "created_date, last_modified_date FROM product WHERE 1=1";
 
         Map<String, Object> map = new HashMap<>();
 
         if (productQueryParams.getCategory() != null) {
-            sql = sql + " AND category = :category";
+            sql += " AND category = :category";
             map.put("category", productQueryParams.getCategory().name());
         }
 
-        if (productQueryParams != null) {
-            sql = sql + " AND product_name LIKE :search";
+        if (productQueryParams.getSearch() != null) {
+            sql += " AND product_name LIKE :search";
             map.put("search", "%" + productQueryParams.getSearch() + "%");
         }
 
-        List<Product> productList = namedParameterJdbcTemplate.query(sql, map, new ProductRowMapper());
+        sql += " ORDER BY " + productQueryParams.getOrderBy() + " " + productQueryParams.getSort();
 
-        return productList;
-
+        return namedParameterJdbcTemplate.query(sql, map, new ProductRowMapper());
     }
+
 
     @Override
     public Product getProductById(Integer productId) {
