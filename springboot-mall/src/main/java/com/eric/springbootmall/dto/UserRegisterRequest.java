cae9d634313403646f1e5a3613caaa440a7db993
eric55123 <ken0970754487@gmail>
2024-12-22 04:46:26 +0800

    帳號功能 - 檢查註冊的 email,隱藏返回的密碼

--- springboot-mall/src/main/java/com/eric/springbootmall/dto/UserRegisterRequest.java
+++ springboot-mall/src/main/java/com/eric/springbootmall/dto/UserRegisterRequest.java
@@ -1,11 +1,12 @@
 package com.eric.springbootmall.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public class UserRegisterRequest {
 
     @NotBlank
+    @Email
     private String email;
 
     @NotBlank
