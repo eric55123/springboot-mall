5223d237cb27a2ac5535fb17da6f11b8a409ef04
eric55123 <ken0970754487@gmail>
2024-12-24 03:58:29 +0800

    帳號功能 - 改寫 MD5 加密方法,變更為 bcrypt 方法

--- springboot-mall/src/main/java/com/eric/springbootmall/service/impl/UserServiceImpl.java
+++ springboot-mall/src/main/java/com/eric/springbootmall/service/impl/UserServiceImpl.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
@@ -22,6 +23,9 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserDao userDao;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
         // 檢查註冊 email
@@ -31,9 +35,13 @@ public class UserServiceImpl implements UserService {
             log.warn("該 email {} 已經被註冊", userRegisterRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
-        // 使用 MD5 生成密碼雜湊值
-        String hashedPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
-        userRegisterRequest.setPassword(hashedPassword);
+//        // 使用 MD5 生成密碼雜湊值
+//        String hashedPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+//        userRegisterRequest.setPassword(hashedPassword);
+
+        // 使用 bcrypt 生成密碼雜湊值
+        String hashPassword = passwordEncoder.encode(userRegisterRequest.getPassword());
+        userRegisterRequest.setPassword(hashPassword);
 
         // 創建帳號
         return userDao.createUser(userRegisterRequest);
@@ -55,11 +63,11 @@ public class UserServiceImpl implements UserService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
-        // 使用 MD5 升成雜湊值
-        String hashedPassword = DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+//        // 使用 MD5 升成雜湊值
+//        String hashedPassword = DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
 
         // 比較密碼
-        if (user.getPassword().equals(hashedPassword)) {
+        if (passwordEncoder.matches(userLoginRequest.getPassword(), user.getPassword())) {
             return user;
         } else {
             log.warn("email {} 的密碼不正確", userLoginRequest.getEmail());
